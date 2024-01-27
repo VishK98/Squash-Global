97d8bf896b383428ead4c5b40740d6739e6e10a1
VishK98 <visheshkumar41@gmail.com>
2024-01-27 13:36:47 +0530

    validation added on contact screen

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,11 +8,36 @@ import mapIcon from "../../assets/images/viewmap-icon.webp";
 import message from "../../assets/images/message-icon.png";
 import email from "../../assets/images/email-icon1.png";
 import uae from "../../assets/images/uae-hq-icon.png";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 
 import "./Contact.css";
 
 function Contact() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      state: '',
+      contactNumber: '',
+      selectedService: 'selected',
+      additionalInfo: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('Name is required'),
+      email: Yup.string().required('Invalid email format').required('Email is required'),
+      state: Yup.string().required('Invalid state format').required('State is required'),
+      contactNumber: Yup.string().required('Invalid contactNumber format').required('Contact is required'),
+      service: Yup.string().required('Invalid contactNumber format').required('Service is required'),
+      // additionalInfo: Yup.string().required('Invalid additionalInfo format').required('Additional info is required'),
+      // Add more validation for other fields
+    }),
+    onSubmit: (values) => {
+      // Handle form submission logic here
+      console.log('Form submitted:', values);
+    },
+  });
   // Create a state to manage the selected value
   const [selectedService, setSelectedService] = useState("selected");
 
@@ -51,7 +76,7 @@ function Contact() {
           </div>
         </div>
       </div>
-      <div className="container mt-3 p-lg-0 mb-5 pb-5">
+      <div className="container mt-3 mb-5">
         <div className="row align-items-center">
           <div className="col-lg-12">
             <div className="inner_section_img position-relative hidden">
@@ -63,7 +88,7 @@ function Contact() {
             </div>
           </div>
         </div>
-        <div className="row mt-5 pb-5">
+        <div className="row mt-5 ">
           <div className="col-lg-5 pt-lg-5">
             <div className="mt-4 mt-lg-0 address-info topslide">
               <div className="hidden">
@@ -154,112 +179,158 @@ function Contact() {
           <div className="col-lg-6 offset-lg-1 pt-5">
             <div
               className="formcol main-contact-form topslide"
-              style={{ transform: "translate(0px, 0px)", opacity: 1 }}
             >
-              <div className="hidden">
-                <h1 style={{ fontWeight: "bold" }} ml-5>
-                  Catch up with us
-                </h1>
-              </div>
-              <div id="contactForm">
-                <form className="mt-5">
-                  <label>Full Name</label> <br></br>
-                  <input
-                    type="text"
-                    name="name"
-                    // value={this.state.name}
-                    // onChange={this.handleChange}
-                    placeholder="Type here"
-                    autoComplete="off"
-                    className="form-field mb-3"
-                  />
-                  <label>Email</label> <br></br>
-                  <input
-                    type="text"
-                    name="name"
-                    // value={this.state.name}
-                    // onChange={this.handleChange}
-                    placeholder="Type here"
-                    autoComplete="off"
-                    className="form-field mb-3"
-                  />
-                  <label>Country</label> <br></br>
-                  <input
-                    type="text"
-                    name="name"
-                    // value={this.state.name}
-                    // onChange={this.handleChange}
-                    placeholder="Type here"
-                    autoComplete="off"
-                    className="form-field mb-3"
-                  />
-                  <label>Contact number</label> <br></br>
-                  <input
-                    type="text"
-                    name="name"
-                    // value={this.state.name}
-                    // onChange={this.handleChange}
-                    placeholder="+91 7352690391"
-                    autoComplete="off"
-                    className="form-field mb-3"
-                  />
-                  <div>
-                    <label htmlFor="service">Choose a service:</label>
-                    <select
-                      name="service"
-                      id="service"
-                      class="custom-select"
-                      value={selectedService}
-                      onChange={handleServiceChange}
-                    >
-                      <option value="selected">Select</option>
-                      <option value="Brand Discovery and Strategy">
-                        Brand Discovery and Strategy
-                      </option>
-                      <option value="Social Media Strategy">
-                        Social Media Strategy
-                      </option>
-                      <option value="Data and Insights">
-                        Data and Insights
-                      </option>
-                      <option value="Creative and Content">
-                        Creative and Content
-                      </option>
-                      <option value="Design and Development">
-                        Design and Development
-                      </option>
-                      <option value="Organic and Paid Marketing">
-                        Organic and Paid Marketing
-                      </option>
-                      <option value="Other">Other</option>
-                    </select>
+              <h1 style={{ fontWeight: "bold" }} ml-5>
+                Catch up with us
+              </h1>
+
+              <form className="mt-5" onSubmit={formik.handleSubmit}>
+                <label>Full Name<sup style={{color: "red", marginLeft: "5px", fontSize: "12px"}}>*</sup></label> <br></br>
+                <input
+                  type="text"
+                  name="name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  placeholder="Type here"
+                  autoComplete="off"
+                  className="form-field mb-3"
+                />
+                {formik.errors.name && (
+                  <div className="error-container position-relative">
+                    <div className="error-tooltip">
+                      {formik.errors.name}
+                      <div className="arrow"></div>
+                    </div>
                   </div>
-                  <label>Write here something to add</label> <br></br>
-                  <input
+                )}
+                <label>Email<sup style={{color: "red", marginLeft: "5px", fontSize: "12px"}}>*</sup></label> <br></br>
+                <input
+                  type="text"
+                  name="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  placeholder="Type here"
+                  autoComplete="off"
+                  className="form-field mb-3"
+                />
+                {formik.errors.email && (
+                  <div className="error-container position-relative">
+                    <div className="error-tooltip">
+                      {formik.errors.email}
+                      <div className="arrow"></div>
+                    </div>
+                  </div>
+                )}
+                <label>State<sup style={{color: "red", marginLeft: "5px", fontSize: "12px"}}>*</sup></label> <br></br>
+                <input
+                  type="text"
+                  name="state"
+                  value={formik.values.state}
+                  onChange={formik.handleChange}
+                  placeholder="Type here"
+                  autoComplete="off"
+                  className="form-field mb-3"
+                />
+                {formik.errors.state && (
+                  <div className="error-container position-relative">
+                    <div className="error-tooltip">
+                      {formik.errors.state}
+                      <div className="arrow"></div>
+                    </div>
+                  </div>
+                )}
+                <label>Contact Number<sup style={{color: "red", marginLeft: "5px", fontSize: "12px"}}>*</sup></label> <br></br>
+                <input
+                  type="text"
+                  name="contactNumber"
+                  value={formik.values.contactNumber}
+                  onChange={formik.handleChange}
+                  placeholder="Type here"
+                  autoComplete="off"
+                  className="form-field mb-3"
+                />
+                {formik.errors.contactNumber && (
+                  <div className="error-container position-relative">
+                    <div className="error-tooltip">
+                      {formik.errors.contactNumber}
+                      <div className="arrow"></div>
+                    </div>
+                  </div>
+                )}
+                <div>
+                  <label htmlFor="service">Choose a service <sup style={{color: "red", marginLeft: "5px", fontSize: "12px"}}>*</sup></label>
+                  <select
                     type="text"
-                    name="name"
-                    // value={this.state.name}
-                    // onChange={this.handleChange}
+                    name="service"
+                    value={formik.values.service}
+                    onChange={formik.service}
                     placeholder="Type here"
                     autoComplete="off"
-                    className="form-field mb-5"
-                  />
-                  <div
-                    id="contactSubmitBtn"
-                    className="d-flex justify-content-center"
+                    className="form-field mb-3"
                   >
-                    <input style={{fontSize: "20px", fontWeight: "bold"}}
-                      type="submit"
-                      name="submit"
-                      value="Send it"
-                      class="submit"
-                    />
+                    <option value="selected">Select</option>
+                    <option value="Brand Discovery and Strategy">
+                      Brand Discovery and Strategy
+                    </option>
+                    <option value="Social Media Strategy">
+                      Social Media Strategy
+                    </option>
+                    <option value="Data and Insights">
+                      Data and Insights
+                    </option>
+                    <option value="Creative and Content">
+                      Creative and Content
+                    </option>
+                    <option value="Design and Development">
+                      Design and Development
+                    </option>
+                    <option value="Organic and Paid Marketing">
+                      Organic and Paid Marketing
+                    </option>
+                    <option value="Other">Other</option>
+                  </select>
+                </div>
+                {formik.errors.service && (
+                  <div className="error-container position-relative">
+                    <div className="error-tooltip">
+                      {formik.errors.service}
+                      <div className="arrow"></div>
+                    </div>
                   </div>
-                </form>
-              </div>
+                )}
+                <label>Write here something to add</label> <br></br>
+                <input
+                  type="text"
+                  name="additionalInfo"
+                  value={formik.values.additionalInfo}
+                  onChange={formik.handleChange}
+                  placeholder="Type here"
+                  autoComplete="off"
+                  className="form-field mb-3"
+                />
+                {/* {formik.errors.additionalInfo && (
+                  <div className="error-container position-relative">
+                    <div className="error-tooltip">
+                      {formik.errors.additionalInfo}
+                      <div className="arrow"></div>
+                    </div>
+                  </div>
+                )} */}
+                <div className="d-flex justify-content-center text-center"
+                >
+                  <input style={{ fontSize: "20px", fontWeight: "bold" }}
+                    type="submit"
+                    name="submit"
+                    value="Send it"
+                    class="submit"
+                  />
+                </div>
+              </form>
+
             </div>
           </div>
         </div>
+
       </div>
       <ScrollToTopButton />
     </>
