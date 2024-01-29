f6cc1e6da59baeca1421f0f3afabfd9e666a6d50
VishK98 <visheshkumar41@gmail.com>
2024-01-29 20:22:33 +0530

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -186,12 +186,12 @@ export default function NavBar() {
                   </Link>
                 </li>
                 <li className="nav-item">
-                  {/* <Link className="nav-link" to="/contact">
+                  <Link className="nav-link" to="/contact">
                     Contact
-                  </Link> */}
-                   <Link className="nav-link" to="/popup">
-                    Popup
                   </Link>
+                   {/* <Link className="nav-link" to="/popup">
+                    Popup
+                  </Link> */}
                 </li>
               </ul>
             </div>
