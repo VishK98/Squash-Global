fec2d1d44d7111a999a3bd6c2f352cc008fe26b3
VishK98 <90708676+VishK98@users.noreply.github.com>
2024-02-03 17:41:52 +0530

    Update PartnerSlider.js

--- src/components/Home/PartnerSlider.js
+++ src/components/Home/PartnerSlider.js
@@ -49,7 +49,7 @@ function PartnerSlider() {
                 settings: {
                     slidesToShow: 1,
                     centerMode: true,
-                    centerPadding: '70px', // Adjust as needed
+                    centerPadding: '75px', // Adjust as needed
                 }
             }
         ]
