17f9de2187b0d97c23f50c4684be29cd66b77be5
VishK98 <visheshkumar41@gmail.com>
2024-02-03 20:09:40 +0530

    slider

--- src/components/Home/PartnerSlider.js
+++ src/components/Home/PartnerSlider.js
@@ -4,17 +4,18 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import "./PartnerSlider.css";
 import GAP from "../../assets/images/Partners/GAP.png";
-import Aditya from "../../assets/images/Partners/aditya-birla.gif";
-import Ashirwaad from "../../assets/images/Partners/ashirwaad.jpeg";
 import Asianpaint from "../../assets/images/Partners/asianpaint.jpg";
 import CEAT from "../../assets/images/Partners/ceat.jpg";
 import Dolby from "../../assets/images/Partners/dolby-atmos.webp";
 import Hira from "../../assets/images/Partners/hira.png";
 import Kotak from "../../assets/images/Partners/kotak.png";
 import Loreal from "../../assets/images/Partners/loreal.png";
-import Paypal from "../../assets/images/Partners/paypal.png";
-import Playstation from "../../assets/images/Partners/playstation.jpg";
 import Yes from "../../assets/images/Partners/yes.png";
+import Panasonic from "../../assets/images/Partners/panasonic.webp";
+import DHL from "../../assets/images/Partners/DHL.webp";
+import Square from "../../assets/images/Partners/square.png";
+import Square1 from "../../assets/images/Partners/square1.png";
+import Cencora from "../../assets/images/Partners/cencora.png";
 
 function PartnerSlider() {
     const settings = {
@@ -55,17 +56,17 @@ function PartnerSlider() {
         ]
     };
 
-    const images = [GAP, Aditya, Ashirwaad, Asianpaint, CEAT, Dolby, Playstation, Hira, Kotak, Loreal, Paypal, Yes];
+    const images = [GAP, Asianpaint, CEAT, Dolby, Hira, Kotak, Loreal, Yes, Panasonic, DHL, Square, Square1, Cencora];
 
     return (
         // <div className='container-fluid'>
-            <Slider {...settings}>
-                {images.map((image, index) => (
-                    <div key={index}>
-                        <img className='slide-image' src={image} alt={`Partner ${index + 1}`} />
-                    </div>
-                ))}
-            </Slider>
+        <Slider {...settings}>
+            {images.map((image, index) => (
+                <div key={index}>
+                    <img className='slide-image' src={image} alt={`Partner ${index + 1}`} />
+                </div>
+            ))}
+        </Slider>
         // </div>
     )
 }
