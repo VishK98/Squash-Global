6f311003113c9d30b92e3874b5fcaa8f2dd49147
VishK98 <visheshkumar41@gmail.com>
2024-02-05 20:29:15 +0530

    button fixed

--- src/components/Sector/Sector.js
+++ src/components/Sector/Sector.js
@@ -18,127 +18,127 @@ import Expert from "../../assets/images/connect-with-our-experts.png";
 import { Link } from "react-router-dom";
 
 function Sector() {
-  const box =[
+  const box = [
     {
-      title : "Energy and Utilities",
+      title: "Energy and Utilities",
       url: energy
     },
     {
-      title : "FMCG",
+      title: "FMCG",
       url: FMCG
     },
     {
-      title : "Consumer Goods",
+      title: "Consumer Goods",
       url: Consumer
     },
     {
-      title : "BFSI",
+      title: "BFSI",
       url: BFSI
     },
     {
-      title : "Automotive",
+      title: "Automotive",
       url: Automotive
     },
     {
-      title : "Retails",
+      title: "Retails",
       url: Retails
     },
     {
-      title : "Food and Beverages",
+      title: "Food and Beverages",
       url: foods
     },
     {
-      title : "Tourism and Hospitality",
+      title: "Tourism and Hospitality",
       url: Hospitality
     },
     {
-      title : "Media and Entertainment",
+      title: "Media and Entertainment",
       url: Media
     },
     {
-      title : "Pharmaceutical",
+      title: "Pharmaceutical",
       url: Pharmaceutical
     },
     {
-      title : "Telecom",
+      title: "Telecom",
       url: Telecom
     },
     {
-      title : "Fashion And Lifestyle",
+      title: "Fashion And Lifestyle",
       url: Fashion
     },
   ];
   return (
-      <>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="route-text">
-                <ol class="breadcrumb bg-transparent px-0 route-text mt-4">
-                  <li>
-                    <a
-                      href="/"
-                      style={{ color: "grey", textDecoration: "none" }}
-                    >
-                      Home /
-                    </a>
-                  </li>
-                  <li
-                    class="breadcrumb-item active"
-                    aria-current="page"
-                    style={{ marginLeft: "8px", color: "grey" }}
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="route-text">
+              <ol class="breadcrumb bg-transparent px-0 route-text mt-4">
+                <li>
+                  <a
+                    href="/"
+                    style={{ color: "grey", textDecoration: "none" }}
                   >
-                    Sector
-                  </li>
-                </ol>
-              </div>
+                    Home /
+                  </a>
+                </li>
+                <li
+                  class="breadcrumb-item active"
+                  aria-current="page"
+                  style={{ marginLeft: "8px", color: "grey" }}
+                >
+                  Sector
+                </li>
+              </ol>
             </div>
           </div>
         </div>
-        <div className="p-lg-0 mb-3">
-          <div className="container">
-            <div className="row">
-              <h1 className="industries-page-title">
-                Helping businesses run
-                <br /> the World Better
-              </h1>
-              <div className="col-lg-12">
-                <picture>
-                  <img
-                    src={industriesBanner}
-                    className="img-fluid"
-                    width="100%"
-                    alt="Helping businesses run the World Better"
-                  />
-                </picture>
-                <p style={{ fontSize: "20px" }}>
-                  We understand businesses and aid them in solving problems for
-                  the world– a pivotal trait in the ever-evolving digital
-                  landscape. With each sector presenting unique challenges, our
-                  cross-industry exposure has enabled us to cultivate a holistic
-                  understanding of user behaviour and preferences.
-                </p>
-                <p style={{ fontSize: "20px" }}>
-                  From retail and technology to utilities and FMCG, we adeptly
-                  navigate complex landscapes. Immersed in industry intricacies,
-                  we craft distinct campaigns, transcending one-size-fits-all
-                  approaches.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      </div>
+      <div className="p-lg-0 mb-3">
         <div className="container">
-          <h2 className="box-title">
-            Some of the industry sectors we partner with
-          </h2>
-          <div
-            className="row"
-            style={{ justifyContent: "space-between", marginRight: "5px" }}
-          >
-            {box.map((image, index) => (
-              // console.log('data', image)
-              <div className="col-lg-2 col-md-6 col-6 mt-3">
+          <div className="row">
+            <h1 className="industries-page-title">
+              Helping businesses run
+              <br /> the World Better
+            </h1>
+            <div className="col-lg-12">
+              <picture>
+                <img
+                  src={industriesBanner}
+                  className="img-fluid"
+                  width="100%"
+                  alt="Helping businesses run the World Better"
+                />
+              </picture>
+              <p style={{ fontSize: "20px" }}>
+                We understand businesses and aid them in solving problems for
+                the world– a pivotal trait in the ever-evolving digital
+                landscape. With each sector presenting unique challenges, our
+                cross-industry exposure has enabled us to cultivate a holistic
+                understanding of user behaviour and preferences.
+              </p>
+              <p style={{ fontSize: "20px" }}>
+                From retail and technology to utilities and FMCG, we adeptly
+                navigate complex landscapes. Immersed in industry intricacies,
+                we craft distinct campaigns, transcending one-size-fits-all
+                approaches.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <h2 className="box-title">
+          Some of the industry sectors we partner with
+        </h2>
+        <div
+          className="row"
+          style={{ justifyContent: "space-between", marginRight: "5px" }}
+        >
+          {box.map((image, index) => (
+            // console.log('data', image)
+            <div className="col-lg-2 col-md-6 col-6 mt-3">
               <div className="boxes">
                 <div className="col">
                   <div className="industries-four-boxes h-100">
@@ -147,193 +147,70 @@ function Sector() {
                   </div>
                 </div>
               </div>
-            </div>       
+            </div>
           ))}
-            {/* <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img
-                      className="box-img"
-                      src={energy}
-                      alt="Energy and Utilities"
-                    />
-                    <p className="box-text">
-                      Energy and
-                      <br />
-                      Utilities
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img className="box-img" src={FMCG} alt="FMGC" />
-                    <p className="mt-2 box-text">FMGC</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img className="box-img" src={Consumer} alt="Consumer" />
-                    <p className="mt-2 box-text">
-                      Consumer
-                      <br />
-                      Goods
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img className="box-img" src={BFSI} alt="Consumer" />
-                    <p className="mt-2 box-img">BFSI</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img
-                      className="box-img"
-                      src={Automotive}
-                      alt="Automotive"
-                    />
-                    <p className="mt-2 box-img">Automotive</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img className="box-img" src={Retails} alt="Retails" />
-                    <p className="mt-2 box-img">Retails</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img className="box-img" src={foods} alt="foods" />
-                    <p className="box-img">
-                      Food and
-                      <br /> Beverages
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img
-                      className="box-img"
-                      src={Hospitality}
-                      alt="Hospitality"
-                    />
-                    <p className="box-img">
-                      Tourism and
-                      <br /> Hospitality
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img className="box-img" src={Media} alt="Media" />
-                    <p className="box-img">
-                      Media and <br />
-                      Entertainment
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100">
-                    <img
-                      className="box-img"
-                      src={Pharmaceutical}
-                      alt="Pharmaceutical"
-                    />
-                    <p className="box-img">Pharmaceutical</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100 ">
-                    <img className="box-img" src={Telecom} alt="Telecom" />
-                    <p className="box-img">Telecom</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 col-6 mt-3">
-              <div className="boxes">
-                <div className="col">
-                  <div className="industries-four-boxes h-100 ">
-                    <img className="box-img" src={Fashion} alt="Fashion" />
-                    <p className="box-img">
-                      Fashion and
-                      <br /> Lifestyle
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div> */}
+        </div>
+      </div>
+
+
+      {/* <div className="container-fluid position-relative">
+        <img
+          src={Expert}
+          className="img-fluid mt-lg-4 mt-3"
+          width="100%"
+          alt="Ready to Solve Something Together"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div className="position-absolute top-50 start-50 ">
+          <div className="container" style={{marginLeft: "30px"}}>
+          <h3 style={{color: "white"}}>Ready to Solve Something Together?</h3>
+          <p style={{color: "white"}} >
+            If you're looking to enhance your SEO, PPC, and broader digital
+            marketing strategies, our team is excited to engage with you.
+            Let's start a conversation!
+          </p>
+          <a href="#" className="btn-5">Request a call back </a>
           </div>
         </div>
-        <div className="mt-4" style={{ position: "relative", width: "100%", height: "100%" }}>
-          <img
-            src={Expert}
-            className="img-fluid"
-            width="100%"
-            alt="Ready to Solve Something Together"
-            style={{ width: "100%", height: "auto" }}
-          />
-          <div className="overlap-text"
-          >
-            <div className="col-md-12 col-lg-8 col-12">
-            <h3>Ready to Solve Something Together?</h3>
-            <p>
+      </div> */}
+      <div className="position-relative">
+        <img
+          src={Expert}
+          className="img-fluid mt-lg-4 mt-3"
+          width="100%"
+          alt="Ready to Solve Something Together"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div className="position-absolute top-50 start-50 d-none d-md-block">
+          <div className="container" style={{ marginLeft: "30px" }}>
+            <h3 style={{ color: "white" }}>Ready to Solve Something Together?</h3>
+            <p style={{ color: "white" }}>
               If you're looking to enhance your SEO, PPC, and broader digital
-              marketing strategies, our team is excited to engage with you.
-              Let's start a conversation!
+              marketing strategies, our team is excited to engage with you. Let's
+              start a conversation!
             </p>
-           <button className="button">
-           <Link to="/contact" style={{textDecoration: "none"}} >
+            {/* <a href="#" className="btn-5">
               Request a call back
-            </Link>
-           </button>
+            </a> */}
+            <div>
+              <button class="req-btn"><i class="animation"></i>Request a call back<i class="animation"></i>
+              </button>
             </div>
           </div>
         </div>
-        <ScrollToTopButton />
-      </> 
+      </div>
+        <div className="hide-web mt-3 " >
+          <h3 style={{ color: "black" }}>Ready to Solve Something Together?</h3>
+          <p style={{ color: "black" }}>
+            If you're looking to enhance your SEO, PPC, and broader digital
+            marketing strategies, our team is excited to engage with you. Let's
+            start a conversation!
+          </p>
+          <button class="req-btn"><i class="animation"></i>Request a call back<i class="animation"></i>
+              </button>
+        </div>
+      <ScrollToTopButton />
+    </>
   );
 }
 
