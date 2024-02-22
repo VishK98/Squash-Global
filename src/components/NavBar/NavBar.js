b8e3e63c82be5bff98eae6ba078d36b8e997d19b
VishK98 <visheshkumar41@gmail.com>
2024-02-22 17:45:44 +0530

    new logo added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 // import logo from "../../assets/images/logo.png";
-import logo from "../../assets/logo22.png";
+import logo from "../../assets/new-logo.png";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 
