c9091fc2b5ac2a956a17ab23916adf17bbb3cbc3
VishK98 <visheshkumar41@gmail.com>
2024-02-25 20:36:47 +0530

    clients logo

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -9,18 +9,33 @@ import client5 from "../../assets/images/clients/client-5.png";
 import client6 from "../../assets/images/clients/client-6.png";
 import client7 from "../../assets/images/clients/client-7.png";
 import client8 from "../../assets/images/clients/client-8.png";
-// import ourClients from "../../assets/images/our-clients.png";
 import ourClients from "../../assets/images/Client Banner.jpg";
-
 import TAWVideo from "../../assets/squash-video.mp4";
 import AnimatedText from '../Animations/TextAnimation';
 import ImageAnimationUnfold from "../Animations/ImageAnimationUnfold";
 import ImageAnimationFade from "../Animations/ImageAnimationFade";
+import newClient from "../../assets/images/client logos/6.png";
+import newClient1 from "../../assets/images/client logos/10.png";
+import newClient2 from "../../assets/images/client logos/11.png";
+import newClient3 from "../../assets/images/client logos/12.png";
+import newClient4 from "../../assets/images/client logos/13.png";
+import newClient5 from "../../assets/images/client logos/14.png";
+import newClient6 from "../../assets/images/client logos/15.png";
+import newClient7 from "../../assets/images/client logos/16.png";
+import newClient8 from "../../assets/images/client logos/17.png";
+import newClient9 from "../../assets/images/client logos/18.png";
+import newClient10 from "../../assets/images/client logos/19.png";
+import newClient11 from "../../assets/images/client logos/2.png";
+import newClient12 from "../../assets/images/client logos/20.png";
+import newClient13 from "../../assets/images/client logos/3.png";
+import newClient14 from "../../assets/images/client logos/4.png";
+import newClient15 from "../../assets/images/client logos/5.png";
+
 
 
 
 function Work() {
-  const clientImage = [client1, client2, client3, client4, client5, client6, client7, client8, client2, client3, client4, client5, client6, client7, client1, client3];
+  const clientImage = [newClient, newClient1, newClient2, newClient3, newClient4, newClient5, newClient6, newClient7, newClient8, newClient9, newClient10, newClient11, newClient12, newClient13, newClient14, newClient15];
 
   return (
     <>
