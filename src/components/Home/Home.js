2952185ef19499414e2985e3e870099b0430cbc8
Vishal Upadhyay <mevishalupadhyay@gmail.com>
2024-02-28 14:08:21 +0530

    Fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -654,7 +654,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="container">
+      {/* <div className="container">
         <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-lg-6 col-md-6 col-12">
@@ -872,7 +872,8 @@ function Home() {
             </button>
           </div>
         </form>
-      </div>
+      </div> */}
+      <div> <form> <div class="visme_d" data-title="Untitled Project" data-url="ojewgd87-untitled-project?fullPage=true" data-domain="forms" data-full-page="true" data-min-height="100vh" data-form-id="28772"></div><script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script> </form> </div>
       <ScrollToTopButton />
     </>
   );
