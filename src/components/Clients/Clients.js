93634ed7b024085c616b968c8c47aedb5778e494
VishK98 <visheshkumar41@gmail.com>
2024-02-29 22:29:02 +0530

    client-banner fixed

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,16 +1,7 @@
 import React from "react";
 import "./Clients.css";
 import ScrollToTopButton from '../TopButton/TopButton';
-import client1 from "../../assets/images/clients/client-1.png";
-import client2 from "../../assets/images/clients/client-2.png";
-import client3 from "../../assets/images/clients/client-3.png";
-import client4 from "../../assets/images/clients/client-4.png";
-import client5 from "../../assets/images/clients/client-5.png";
-import client6 from "../../assets/images/clients/client-6.png";
-import client7 from "../../assets/images/clients/client-7.png";
-import client8 from "../../assets/images/clients/client-8.png";
-import ourClients from "../../assets/images/Client 1024x587-01.png";
-import TAWVideo from "../../assets/squash-video.mp4";
+import ourClients from "../../assets/images/Client-banner.png";
 import AnimatedText from '../Animations/TextAnimation';
 import ImageAnimationUnfold from "../Animations/ImageAnimationUnfold";
 import ImageAnimationFade from "../Animations/ImageAnimationFade";
@@ -32,16 +23,12 @@ import newClient14 from "../../assets/images/client logos/4.png";
 import newClient15 from "../../assets/images/client logos/5.png";
 
 
-
-
 function Work() {
   const clientImage = [newClient, newClient1, newClient2, newClient3, newClient4, newClient5, newClient6, newClient7, newClient8, newClient9, newClient10, newClient11, newClient12, newClient13, newClient14, newClient15];
 
   return (
     <>
-      <div className="full-screen-image">
-        <ImageAnimationUnfold src={ourClients} alt="Image not found" style={{ width: "100%", height: "100%", objectFit: "fill" }} />
-      </div>
+    <ImageAnimationUnfold src={ourClients} />
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -50,11 +37,11 @@ function Work() {
           </div>
         </div>
       </div>
-      <div className="container">
+      <div className="container mt-lg-5 mt-5">
         <AnimatedText text={<h4 className="guest-text mb-4">
           Guests are always welcome at our establishment, and we aim to provide them with a memorable experience.
         </h4>} animation='fade-in' />
-        <AnimatedText text={<h1 className="text-center partnership-text mt-3 mt-lg-4">Building Strong Relationships with Our Valued Clients</h1>
+        <AnimatedText text={<h1 className="text-center partnership-text mt-3 mt-lg-5 mb-lg-5">Building Strong Relationships with Our Valued Clients</h1>
         } animation='slide-up' />
         <div
           className="row mt-3 mt-lg-4"
@@ -71,12 +58,6 @@ function Work() {
           ))}
         </div>
       </div>
-      {/* <div className="video-box">
-        <video autoPlay loop muted playsInline >
-          <source src={TAWVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div> */}
       <ScrollToTopButton />
     </>
   );
