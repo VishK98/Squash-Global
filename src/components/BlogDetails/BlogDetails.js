7140919396da55aa383d6a6c174106d1d06ffb1e
Vishal Upadhyay <mevishalupadhyay@gmail.com>
2024-04-08 18:15:14 +0530

    Fixed

--- src/components/BlogDetails/BlogDetails.js
+++ src/components/BlogDetails/BlogDetails.js
@@ -1,107 +1,186 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./BlogDetails.css";
-import ScrollToTopButton from '../TopButton/TopButton';
+import ScrollToTopButton from "../TopButton/TopButton";
 import industriesBanner from "../../assets/images/taw-blog-banner1.png";
 import blogImage10 from "../../assets/images/taw-blog-banner1.png";
 import blogImage4 from "../../assets/images/taw-blog-banner1.png";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 function BlogDetails() {
-    const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    const toggleVisibility = () => {
-        if (window.pageYOffset > 300) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
     };
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', toggleVisibility);
-
-        return () => {
-            window.removeEventListener('scroll', toggleVisibility);
-        };
-    }, []);
-    return (
-        <>
-            <title>How Changes in Social Media Algorithms - The Agency Way (TAW)</title>
-        <meta name="description" content="Stay ahead with the latest insights! Get The Latest Blogs - The Agency Way (TAW) for expert tips, trends, and strategies to propel your business forward." />
-        <link rel="canonical" href="https://taw.agency/blog/how-changes-in-social-media-algorithms" />
-
-            <div className="container col-lg-10 mt-3">
-                <h1 className="text-center">How Changes in Social Media Algorithms Are Influencing Content Strategy and What Marketers Need to Know to Stay Ahead</h1>
-                <img
-                    src={industriesBanner}
-                    className="img-fluid mt-3"
-                    width="100%"
-                    alt="Helping businesses run the World Better"
-                />
-            </div>
-            <div className="container mt-5 col-lg-8">
-                <p>
-                    Changes in social media algorithms have a profound impact on how content is distributed and seen on platforms. To stay ahead in the dynamic landscape of social media, marketers need to understand these changes and adapt their content strategies accordingly. Here's a comprehensive overview:
-                </p>
-           <h3>1. Shift from Chronological to Algorithmic Feeds:</h3>
-           <p>Content is no longer displayed in chronological order. Algorithms prioritize content based on relevance and user engagement.</p>
-           <p>Focus on creating high-quality, engaging content that prompts user interactions and encourages them to like, comments and shares to improve visibility</p>
-            </div>
-            <div className="container mt-2 col-lg-8">
-                <img
-                    src={blogImage10}
-                    className="img-fluid mt-3 mb-3"
-                    width="100%"
-                    alt="Helping businesses run the World Better"
-                />
-                <h3>2. Emphasis on User Engagement Metrics:</h3>
-                <p>Algorithms consider engagement metrics to assess content quality and relevance.</p>
-                <p>Craft content that encourages meaningful interactions. Ask questions, run polls, and create shareable content.</p>
-                <h3>3. Video Content Takes Center Stage:</h3>
-                <p>Platforms prioritize video content over static posts due to higher user engagement.</p>
-                <p>Invest in video content. Create engaging, short-form videos optimized for each platform.</p>
-                <h3>4. Authenticity and Transparency Are Valued:</h3>
-                <p>Algorithms favor authentic content. Users engage more with genuine, transparent brand communications.</p>
-                <p>Showcase the human side of your brand. Share behind-the-scenes content, user testimonials, and real stories.</p>
-                <h3>5. Stories Gain Prominence:</h3>
-                <p>Algorithms prioritize ephemeral content, such as Stories, due to its popularity.</p>
-                <p>Integrate Stories into your content mix. Use features like polls, quizzes, and countdowns for interactive engagement.</p>
-                <h3>6. Community Building Matters:</h3>
-                <p>Algorithms prioritize content that fosters community engagement.</p>
-                <p>Build a community around your brand. Encourage user-generated content, respond to comments, and create content that resonates with your audience.</p>
-                <h3>7. Algorithm-Specific Optimization:</h3>
-                <p>Each platform has unique algorithms. What works on one may not work on another.</p>
-                <p>Tailor your content for each platform. Understand the nuances of algorithms on Instagram, Facebook, Twitter, etc.</p>
-                <img
-                    src={blogImage4}
-                    className="img-fluid mt-3 mb-3"
-                    width="100%"
-                    alt="Helping businesses run the World Better"
-                />
-                <h3>8. Diversification of Content Formats:</h3>
-                <p>Algorithms reward varied content formats. A mix of images, videos, carousels, and text posts can boost visibility.</p>
-                <p>Diversify your content. Experiment with different formats to keep your audience engaged.</p>
-                <h3>9. Responsive Timing and Consistency:</h3>
-                <p>Consistency in posting and responsiveness to comments are considered by algorithms.</p>
-                <p>Schedule posts for optimal times, engage with your audience promptly, and maintain a consistent posting schedule.</p>
-                <h3>10. Adaptation to Evolving Algorithms:</h3>
-                <p>Algorithms are subject to change. Staying informed about updates is crucial.</p>
-                <p>Stay updated with platform announcements. Be ready to tweak your strategy based on algorithm changes.</p>
-                <p>In summary, the key to staying ahead in the ever-evolving landscape of social media is a dynamic and adaptable content strategy. Monitor analytics, stay informed about algorithm changes, and consistently create content that resonates with your audience. The ability to adapt swiftly is the cornerstone of a successful social media marketing strategy.</p>
-            </div>
-            <ScrollToTopButton />
-
-
-        </>
-
-    );
+  }, []);
+  return (
+    <>
+      <helmet>
+        <title>
+          How Changes in Social Media Algorithms - The Agency Way (TAW)
+        </title>
+        <meta
+          name="description"
+          content="Stay ahead with the latest insights! Get The Latest Blogs - The Agency Way (TAW) for expert tips, trends, and strategies to propel your business forward."
+        />
+        <link
+          rel="canonical"
+          href="https://taw.agency/blog/how-changes-in-social-media-algorithms"
+        />
+      </helmet>
+      <div className="container col-lg-10 mt-3">
+        <h1 className="text-center">
+          How Changes in Social Media Algorithms Are Influencing Content
+          Strategy and What Marketers Need to Know to Stay Ahead
+        </h1>
+        <img
+          src={industriesBanner}
+          className="img-fluid mt-3"
+          width="100%"
+          alt="Helping businesses run the World Better"
+        />
+      </div>
+      <div className="container mt-5 col-lg-8">
+        <p>
+          Changes in social media algorithms have a profound impact on how
+          content is distributed and seen on platforms. To stay ahead in the
+          dynamic landscape of social media, marketers need to understand these
+          changes and adapt their content strategies accordingly. Here's a
+          comprehensive overview:
+        </p>
+        <h3>1. Shift from Chronological to Algorithmic Feeds:</h3>
+        <p>
+          Content is no longer displayed in chronological order. Algorithms
+          prioritize content based on relevance and user engagement.
+        </p>
+        <p>
+          Focus on creating high-quality, engaging content that prompts user
+          interactions and encourages them to like, comments and shares to
+          improve visibility
+        </p>
+      </div>
+      <div className="container mt-2 col-lg-8">
+        <img
+          src={blogImage10}
+          className="img-fluid mt-3 mb-3"
+          width="100%"
+          alt="Helping businesses run the World Better"
+        />
+        <h3>2. Emphasis on User Engagement Metrics:</h3>
+        <p>
+          Algorithms consider engagement metrics to assess content quality and
+          relevance.
+        </p>
+        <p>
+          Craft content that encourages meaningful interactions. Ask questions,
+          run polls, and create shareable content.
+        </p>
+        <h3>3. Video Content Takes Center Stage:</h3>
+        <p>
+          Platforms prioritize video content over static posts due to higher
+          user engagement.
+        </p>
+        <p>
+          Invest in video content. Create engaging, short-form videos optimized
+          for each platform.
+        </p>
+        <h3>4. Authenticity and Transparency Are Valued:</h3>
+        <p>
+          Algorithms favor authentic content. Users engage more with genuine,
+          transparent brand communications.
+        </p>
+        <p>
+          Showcase the human side of your brand. Share behind-the-scenes
+          content, user testimonials, and real stories.
+        </p>
+        <h3>5. Stories Gain Prominence:</h3>
+        <p>
+          Algorithms prioritize ephemeral content, such as Stories, due to its
+          popularity.
+        </p>
+        <p>
+          Integrate Stories into your content mix. Use features like polls,
+          quizzes, and countdowns for interactive engagement.
+        </p>
+        <h3>6. Community Building Matters:</h3>
+        <p>Algorithms prioritize content that fosters community engagement.</p>
+        <p>
+          Build a community around your brand. Encourage user-generated content,
+          respond to comments, and create content that resonates with your
+          audience.
+        </p>
+        <h3>7. Algorithm-Specific Optimization:</h3>
+        <p>
+          Each platform has unique algorithms. What works on one may not work on
+          another.
+        </p>
+        <p>
+          Tailor your content for each platform. Understand the nuances of
+          algorithms on Instagram, Facebook, Twitter, etc.
+        </p>
+        <img
+          src={blogImage4}
+          className="img-fluid mt-3 mb-3"
+          width="100%"
+          alt="Helping businesses run the World Better"
+        />
+        <h3>8. Diversification of Content Formats:</h3>
+        <p>
+          Algorithms reward varied content formats. A mix of images, videos,
+          carousels, and text posts can boost visibility.
+        </p>
+        <p>
+          Diversify your content. Experiment with different formats to keep your
+          audience engaged.
+        </p>
+        <h3>9. Responsive Timing and Consistency:</h3>
+        <p>
+          Consistency in posting and responsiveness to comments are considered
+          by algorithms.
+        </p>
+        <p>
+          Schedule posts for optimal times, engage with your audience promptly,
+          and maintain a consistent posting schedule.
+        </p>
+        <h3>10. Adaptation to Evolving Algorithms:</h3>
+        <p>
+          Algorithms are subject to change. Staying informed about updates is
+          crucial.
+        </p>
+        <p>
+          Stay updated with platform announcements. Be ready to tweak your
+          strategy based on algorithm changes.
+        </p>
+        <p>
+          In summary, the key to staying ahead in the ever-evolving landscape of
+          social media is a dynamic and adaptable content strategy. Monitor
+          analytics, stay informed about algorithm changes, and consistently
+          create content that resonates with your audience. The ability to adapt
+          swiftly is the cornerstone of a successful social media marketing
+          strategy.
+        </p>
+      </div>
+      <ScrollToTopButton />
+    </>
+  );
 }
 
 export default BlogDetails;
--- src/components/BlogDetails/BlogDetails2.js
+++ src/components/BlogDetails/BlogDetails2.js
@@ -1,94 +1,171 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./BlogDetails.css";
-import ScrollToTopButton from '../TopButton/TopButton';
+import ScrollToTopButton from "../TopButton/TopButton";
 import industriesBanner from "../../assets/images/taw-blog-banner1.png";
 import blogImage10 from "../../assets/images/taw-blog-banner1.png";
 import blogImage4 from "../../assets/images/taw-blog-banner1.png";
-
+import { Helmet } from "react-helmet";
 function BlogDetails2() {
-    const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    const toggleVisibility = () => {
-        if (window.pageYOffset > 300) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
     };
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', toggleVisibility);
-
-        return () => {
-            window.removeEventListener('scroll', toggleVisibility);
-        };
-    }, []);
-    return (
-        <>
-           <title>How Artificial Intelligence is Revolutionizing Advertising - The Agency Way (TAW)</title>
-        <meta name="description" content="How Artificial Intelligence is Revolutionizing Advertising - The Agency Way (TAW) for expert tips, trends, and strategies to propel your business forward." />
-        <link rel="canonical" href="https://taw.agency/blog/how-artificial-intelligence-is-revoluationizing-advertising" />
-            <div className="container col-lg-10 mt-3">
-                <h1 className="text-center">How Artificial Intelligence is Revolutionizing Advertising, From Personalized Targeting to Predictive Analytics</h1>
-                <img
-                    src={industriesBanner}
-                    className="img-fluid mt-3"
-                    width="100%"
-                    alt="Helping businesses run the World Better"
-                />
-            </div>
-            <div className="container mt-5 col-lg-8">
-                <p>
-                Welcome to the future of advertising, where the magic of Artificial Intelligence (AI) is transforming the way, we connect with audiences. TAW is here to guide you through the wonders AI brings to personalized targeting and predictive analytics.
-                </p>
-                <h3>1. Getting Personal:</h3>
-                <p>We used to cast a wide net, hoping our message would land somewhere meaningful. Now, AI helps us dive deep into the digital ocean, finding the exact fish we want. It learns from users' behaviors, tailoring our ads to each individual's tastes and preferences. It's like having a personal shopper for each customer!</p>
-            </div>
-            <div className="container mt-2 col-lg-8">
-                <img
-                    src={blogImage10}
-                    className="img-fluid mt-3 mb-3"
-                    width="100%"
-                    alt="Helping businesses run the World Better"
-                />
-                <h3>2. Ad Creatives that Move:</h3>
-                <p>Earlier Static ads that felt a bit like shouting into the void. With AI enters the dynamic creative optimization. AI watches how users respond and adjusts our ads on the fly. It's like having a super-smart artist who paints the perfect picture for each viewer.</p>
-                <h3>3. Seeing Tomorrow Today:</h3>
-                <p>For many years, marketeers have been peering into the rearview mirror (historical data) for insights. With predictive analytics, it's like having a crystal ball. AI learns patterns and trends, helping us foresee what our audience will want tomorrow. It's not just advertising; it's almost clairvoyance!</p>
-                <h3>4. Ad Buying on Autopilot:</h3>
-                <p>From manually navigating the ad space to Programmatic Advertising which runs on autopilot. AI algorithms make split-second decisions on ad placements, optimizing our strategy in real-time. It's like having a co-pilot who knows the best routes.</p>
-                <h3>5. Chatting with Bots:</h3>
-                <p>Earlier the customer support used to be tedious and slow. AI-powered chatbots step in with immediate resolutions to your concerns. They talk to customers, answer queries, and provide a seamless experience. It’s like having a round-the-clock customer service team without the coffee breaks.</p>
-                <h3>6. Words that Speak Louder:</h3>
-                <p>We have been manually crafting every piece of content to sound more reasonable to our audience. But Thanks to Natural Language Processing, AI helps us generate compelling ad copy that speaks the language of our audience. It's like having Shakespeare at our service, but with algorithms.</p>
-                <h3>7. Faces that Click:</h3>
-                <p>The old way of marketing used to be choosing the right demographics for targeting. With the AI Revolution, Facial recognition steps in. AI analyzes emotions and demographics from images, helping us target with surgical precision. It's like having a detective who knows what people want just by looking at them.</p>
-                <img
-                    src={blogImage4}
-                    className="img-fluid mt-3 mb-3"
-                    width="100%"
-                    alt="Helping businesses run the World Better"
-                />
-                <h3>8. Harmony Across Channels:</h3>
-                <p>The earlier approach seemed to be disjointed efforts across media channels. Now AI integrates data from every channel, giving us a symphony of insights. It's like having a conductor who ensures every instrument plays in harmony.</p>
-                <h3>9. Real-Time Bidding:</h3>
-                <p>For any promotional campaigns, marketeers used to deal with fixed prices. Now with the AI Revolution, Real-time bidding becomes possible. It means AI adjusts prices as the game unfolds. It’s like having a financial wizard who ensures we get the best deal for our ad spots.</p>
-                <h3>10. Defending Your Kingdom:</h3>
-                <p>We no longer have to worry about the constant ad fraud. AI algorithms act as our knights in shining armor, detecting and preventing fraud. It's like having a vigilant guardian who protects our kingdom from invaders.</p>
-                <p>In a nutshell, AI isn't just changing the game; it's creating a whole new playground. As your partners in this journey, we're here to harness the power of AI, making your brand not just heard but truly understood in the digital symphony. It's not just advertising; it's a revolution, and we're at the forefront. Ready to ride the AI wave?</p>
-            </div>
-            <ScrollToTopButton />
-
-
-        </>
-    )
+  }, []);
+  return (
+    <>
+      <Helmet>
+        <title>
+          How Artificial Intelligence is Revolutionizing Advertising - The
+          Agency Way (TAW)
+        </title>
+        <meta
+          name="description"
+          content="How Artificial Intelligence is Revolutionizing Advertising - The Agency Way (TAW) for expert tips, trends, and strategies to propel your business forward."
+        />
+        <link
+          rel="canonical"
+          href="https://taw.agency/blog/how-artificial-intelligence-is-revoluationizing-advertising"
+        />
+      </Helmet>
+      <div className="container col-lg-10 mt-3">
+        <h1 className="text-center">
+          How Artificial Intelligence is Revolutionizing Advertising, From
+          Personalized Targeting to Predictive Analytics
+        </h1>
+        <img
+          src={industriesBanner}
+          className="img-fluid mt-3"
+          width="100%"
+          alt="Helping businesses run the World Better"
+        />
+      </div>
+      <div className="container mt-5 col-lg-8">
+        <p>
+          Welcome to the future of advertising, where the magic of Artificial
+          Intelligence (AI) is transforming the way, we connect with audiences.
+          TAW is here to guide you through the wonders AI brings to personalized
+          targeting and predictive analytics.
+        </p>
+        <h3>1. Getting Personal:</h3>
+        <p>
+          We used to cast a wide net, hoping our message would land somewhere
+          meaningful. Now, AI helps us dive deep into the digital ocean, finding
+          the exact fish we want. It learns from users' behaviors, tailoring our
+          ads to each individual's tastes and preferences. It's like having a
+          personal shopper for each customer!
+        </p>
+      </div>
+      <div className="container mt-2 col-lg-8">
+        <img
+          src={blogImage10}
+          className="img-fluid mt-3 mb-3"
+          width="100%"
+          alt="Helping businesses run the World Better"
+        />
+        <h3>2. Ad Creatives that Move:</h3>
+        <p>
+          Earlier Static ads that felt a bit like shouting into the void. With
+          AI enters the dynamic creative optimization. AI watches how users
+          respond and adjusts our ads on the fly. It's like having a super-smart
+          artist who paints the perfect picture for each viewer.
+        </p>
+        <h3>3. Seeing Tomorrow Today:</h3>
+        <p>
+          For many years, marketeers have been peering into the rearview mirror
+          (historical data) for insights. With predictive analytics, it's like
+          having a crystal ball. AI learns patterns and trends, helping us
+          foresee what our audience will want tomorrow. It's not just
+          advertising; it's almost clairvoyance!
+        </p>
+        <h3>4. Ad Buying on Autopilot:</h3>
+        <p>
+          From manually navigating the ad space to Programmatic Advertising
+          which runs on autopilot. AI algorithms make split-second decisions on
+          ad placements, optimizing our strategy in real-time. It's like having
+          a co-pilot who knows the best routes.
+        </p>
+        <h3>5. Chatting with Bots:</h3>
+        <p>
+          Earlier the customer support used to be tedious and slow. AI-powered
+          chatbots step in with immediate resolutions to your concerns. They
+          talk to customers, answer queries, and provide a seamless experience.
+          It’s like having a round-the-clock customer service team without the
+          coffee breaks.
+        </p>
+        <h3>6. Words that Speak Louder:</h3>
+        <p>
+          We have been manually crafting every piece of content to sound more
+          reasonable to our audience. But Thanks to Natural Language Processing,
+          AI helps us generate compelling ad copy that speaks the language of
+          our audience. It's like having Shakespeare at our service, but with
+          algorithms.
+        </p>
+        <h3>7. Faces that Click:</h3>
+        <p>
+          The old way of marketing used to be choosing the right demographics
+          for targeting. With the AI Revolution, Facial recognition steps in. AI
+          analyzes emotions and demographics from images, helping us target with
+          surgical precision. It's like having a detective who knows what people
+          want just by looking at them.
+        </p>
+        <img
+          src={blogImage4}
+          className="img-fluid mt-3 mb-3"
+          width="100%"
+          alt="Helping businesses run the World Better"
+        />
+        <h3>8. Harmony Across Channels:</h3>
+        <p>
+          The earlier approach seemed to be disjointed efforts across media
+          channels. Now AI integrates data from every channel, giving us a
+          symphony of insights. It's like having a conductor who ensures every
+          instrument plays in harmony.
+        </p>
+        <h3>9. Real-Time Bidding:</h3>
+        <p>
+          For any promotional campaigns, marketeers used to deal with fixed
+          prices. Now with the AI Revolution, Real-time bidding becomes
+          possible. It means AI adjusts prices as the game unfolds. It’s like
+          having a financial wizard who ensures we get the best deal for our ad
+          spots.
+        </p>
+        <h3>10. Defending Your Kingdom:</h3>
+        <p>
+          We no longer have to worry about the constant ad fraud. AI algorithms
+          act as our knights in shining armor, detecting and preventing fraud.
+          It's like having a vigilant guardian who protects our kingdom from
+          invaders.
+        </p>
+        <p>
+          In a nutshell, AI isn't just changing the game; it's creating a whole
+          new playground. As your partners in this journey, we're here to
+          harness the power of AI, making your brand not just heard but truly
+          understood in the digital symphony. It's not just advertising; it's a
+          revolution, and we're at the forefront. Ready to ride the AI wave?
+        </p>
+      </div>
+      <ScrollToTopButton />
+    </>
+  );
 }
 
-export default BlogDetails2
+export default BlogDetails2;
