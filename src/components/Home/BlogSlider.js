4030385ebd599a2b45b249bac84f990d6e442520
Vishal Upadhyay <mevishalupadhyay@gmail.com>
2024-04-12 13:45:12 +0530

    Fixed

--- src/components/Home/BlogSlider.js
+++ src/components/Home/BlogSlider.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './BlogSlider.css';
-import blogImage4 from "../../assets/images/taw-blog-banner1.png";
+import blogImage1 from "../../assets/images/taw-blog-banner1.png";
+import blogImage2 from "../../assets/images/taw-blog-banner2.png";
+import blogImage3 from "../../assets/images/taw-blog-banner3.png";
 import AnimatedFadeImage from '../Animations/ImageAnimationFade';
 import AnimatedText from '../Animations/TextAnimation';
 import rigthArrow from "../../assets/images/Icons/-right-arrow.png";
@@ -31,7 +33,7 @@ const BlogSlider = () => {
               <div className="slide-content ">
                 <div className="col-lg-6 col-md-6 col-12">
                   <div className="box">
-                    <AnimatedFadeImage src={blogImage4} alt="Image not found" />
+                    <AnimatedFadeImage src={blogImage1} alt="Image not found" />
                   </div>
                 </div>
                 <div className='col-lg-5 col-md-5 col-12'>
@@ -51,15 +53,34 @@ const BlogSlider = () => {
               <div className="slide-content ">
                 <div className="col-lg-6 col-md-6 col-12">
                   <div className="box">
-                    <AnimatedFadeImage src={blogImage4} alt="Image not found" />
+                    <AnimatedFadeImage src={blogImage2} alt="Image not found" />
                   </div>
                 </div>
                 <div className='col-lg-5 col-md-5 col-12'>
-                  <AnimatedText text={<h3>Why Images and Videos are Critical for Engagement for Social Media Platforms</h3>
+                  <AnimatedText text={<h3>How Artificial Intelligence is Revolutionizing Advertising, From Personalized Targeting to Predictive Analytics</h3>
                   } animation='slide-up' />
                   <AnimatedText text={<p>
-                    You would have often come across the popular phrase “A picture is worth a thousand words.” However, contrary to this statement is the power of videos that now allows you to dictate your story just in 30 seconds.
-                  </p>} animation='slide-up' />
+                    Welcome to the future of advertising, where the magic of Artificial Intelligence (AI) is transforming the way, we connect with audiences.</p>} animation='slide-up' />
+                  <Link to="/Blog" style={{ textDecoration: "none" }}>
+                    <div className="animated-arrow-btn" style={{ marginLeft: "10px", height: "50px", width: "50px" }}>
+                      <ImageAnimationFade src={rigthArrow} alt="Image not found" />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="slide-item ">
+              <div className="slide-content ">
+                <div className="col-lg-6 col-md-6 col-12">
+                  <div className="box">
+                    <AnimatedFadeImage src={blogImage3} alt="Image not found" />
+                  </div>
+                </div>
+                <div className='col-lg-5 col-md-5 col-12'>
+                  <AnimatedText text={<h3>4 Red Flags to Watch Out in Your Next Content Marketing Strategy</h3>
+                  } animation='slide-up' />
+                  <AnimatedText text={<p>
+                    Discover the top 4 red flags that could be undermining your content marketing strategy. Learn how to identify and address these warning signs to ensure your next campaign is a resounding success.                  </p>} animation='slide-up' />
                   <Link to="/Blog" style={{ textDecoration: "none" }}>
                     <div className="animated-arrow-btn" style={{ marginLeft: "10px", height: "50px", width: "50px" }}>
                       <ImageAnimationFade src={rigthArrow} alt="Image not found" />
