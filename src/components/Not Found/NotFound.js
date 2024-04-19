db6014790e9a9f64435bb5ea5b34429bbc786e08
VishK98 <visheshkumar41@gmail.com>
2024-04-19 16:13:54 +0530

    error page added

--- src/components/Not Found/NotFound.js
+++ src/components/Not Found/NotFound.js
@@ -1,17 +1,20 @@
-// NotFound.js
 import React from 'react';
-import './NotFound.css'; // Import your CSS file for animations
+import './NotFound.css';
+import error from "../../assets/images/error.jpeg";
 
 const NotFound = () => {
   return (
-    <div className="not-found-container">
-      <div className="not-found-message">
-        <h1>404 Not Found</h1>
-        <p>Oops! The page you are looking for does not exist.</p>
+    <div className='container-fluid full-height'>
+      <div className='row full-height'>
+        <div className='col-lg-4 col-md-4 col-12'>
+          <img className='error-image' src={error} alt="Error" />
+        </div>
+        <div className='col-lg-8 col-md-8 col-12 d-flex flex-column justify-content-center align-items-center'>
+          <h1 className='title-text'>AWWW...DON’T CRY.</h1>
+          <p className='error-txt'>It's just a 404 Error!</p>
+          <p className='next-txt'>What you’re looking for may have been misplaced in Long Term Memory.</p>
+        </div>
       </div>
-      <button className="go-back-button" onClick={() => window.history.back()}>
-        Go Back
-      </button>
     </div>
   );
 };
